6653565c86d14336556e08b483da593f1ad09d56
Aryajet <jaked180@gmail.com>
2023-03-20 12:50:07 -0500

    Updated README & type errors in main

--- PhoneNumMnemonics/main.cpp
+++ PhoneNumMnemonics/main.cpp
@@ -7,21 +7,23 @@ using namespace std;
 
 string AlphaToNum(string alpha){
   string Nums;
+  int strLength = alpha.length();
   map<char, char> NumToA = {
       {'a', '2'}, {'b', '2'}, {'c', '2'}, {'d', '3'}, {'e', '3'}, {'f', '3'},
       {'g', '4'}, {'h', '4'}, {'i', '4'}, {'j', '5'}, {'k', '5'}, {'l', '5'},
       {'m', '6'}, {'n', '6'}, {'o', '6'}, {'p', '7'}, {'q', '7'}, {'r', '7'},
       {'s', '7'}, {'t', '8'}, {'u', '8'}, {'v', '8'}, {'w', '9'}, {'x', '9'},
       {'y', '9'}, {'z', '9'}};
-  for (long i = 0; i < alpha.length(); i++){
+  for (int i = 0; i < strLength; i++){
     Nums += NumToA[alpha[i]];
   }
   return Nums;
 }
 
 bool ValidInput(string input) {
-  if (input.length() == 8 && input[3] == '-'){
-    for(int i = 0; i < input.length(); i++){
+  int strLength = input.length();
+  if (strLength == 8 && input[3] == '-'){
+    for(int i = 0; i < strLength; i++){
       if(input[i] == '1' || input[i] == '0'){
         return false;
       }
@@ -34,26 +36,31 @@ bool ValidInput(string input) {
 }
 
 int main() {
-  ifstream fin("ShortList.txt");
-  ofstream fout("output.txt");
-  string lines, input, userKeys;
-  cin >> input;
-  vector<string> word3;
-  vector<string> word4;
-  vector<string> word7;
-  int count3 = 0, count4 = 0, count7 = 0;
+string lines, input, userKeys;
+cin >> input;
+int strLength = input.length();
+
 
   if (ValidInput(input)) {
+    ifstream fin("ShortList.txt");
+    ofstream fout("output.txt");
+    vector<string> word3;
+    vector<string> word4;
+    vector<string> word7;
+    int count3 = 0, count4 = 0, count7 = 0;
     string user3, user4, user7;
-    for (long i = 0; i < input.length(); i++) {
+
+
+    for (long i = 0; i < strLength; i++) {
       if (i < 3) {
         user3 += input[i];
       } else if (i > 3) {
         user4 += input[i];
       }
     }
-
     user7 = user3 + user4;
+
+
     while (getline(fin, lines)) {
       if (user3 == AlphaToNum(lines)){
         count3++;
@@ -69,23 +76,25 @@ int main() {
       }
     }
 
+
     for (int i = 0; i < count3; i++){
       for (int j = 0; j < count4; j++){
         fout << word3.at(i) << " " << word4.at(j) << endl;
       }
     }
-
     for (int i = 0; i < count7; i++){
       fout << word7.at(i) << endl;
     }
 
+
     if (count3 == 0 && count4 == 0 && count7 == 0){
       fout << "No results!" << endl;
     }
-
-  } else{
+  }
+  else{
     cout << "Invalid phone number" << endl;
   }
-  cout << "Done!";
+
+
   return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # PhoneNumMnemonics
  Given a list of words, outputs a file of combinations from an input number in the format of ###-#### from 2-9.
+
+For this project, I had initially thought to itterate over each number iterating over each possible value in a hashmap. this solution is still possible; however, I found it simpler to convert the words in the "ShortList.txt" file to their corresponding number. After doing so I had populated a vector for the lengths of each matching string of numbers. finally for the output this solution loops through each 3 letter word looping through each 4 letter word ending with the 7 letter words.
